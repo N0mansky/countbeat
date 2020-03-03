30afb7f99a3a61095be666e87d3ffb2427b65d87
nomansky <nomansky@nomanskys-MacBook-Pro.local>
2020-03-03 15:47:35 +0800

    Add generated countbeat files

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/n0mansky/countbeat/cmd"
+
+	_ "github.com/n0mansky/countbeat/include"
+)
+
+func main() {
+	if err := cmd.RootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
